a228cef6a82fc0fc07091e1eec2bc2ddc5ac5c7a
Frank Soto <fasfranksoto@gmail.com>
2025-09-16 08:19:08 -0700

    finished strings example

--- data-types/strings.py
+++ data-types/strings.py
@@ -3,5 +3,6 @@
 
 print('Hello World') # prints Hello World to the terminal
 
-print()'Hello World'
-# print(Hello World)
+print()'Hello World' # Syntax Error: indicates that the way the code was written is incorrect. The error message will usually give you an idea of where it's occurring
+print(Hello World) # Syntax Error. In this case, it's because we did not add quotes around Hello World making the program think it was a variable. We will get more into
+# what a variable is later but a variable is used to store data for easy access later.
